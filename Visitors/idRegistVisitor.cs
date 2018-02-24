45b97c6532af74df074b2333704e2fb79bab3705
Artem Semenov <sav70792@gmail.com>
2018-02-24 15:25:03 +0300

    описан класс списка имён переменных

---
+++ Visitors/idRegistVisitor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProgramTree;
+
+namespace SimpleLang.Visitors
+{
+    class idRegistVisitor : AutoVisitor
+    {
+        public List<string> idList;
+
+        public override void VisitIdNode(IdNode id)
+        {
+            if (idList.Contains(id.Name)) return; // Если в списке уже есть такое имя: выходим?
+            else idList.Add(id.Name); // Иначе добавляем имя переменной в список?
+        }
+    }
+}
