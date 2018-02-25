5a510bfcce1b3eac615d0af4c271b8c2009fcf9b
savoptik <sav70792@gmail.com>
2018-02-25 20:59:22 +0300

    Список заменён на словарь, всё проверенно и даже работает

--- Visitors/idRegistVisitor.cs
+++ Visitors/idRegistVisitor.cs
@@ -8,12 +8,12 @@ namespace SimpleLang.Visitors
 {
     class idRegistVisitor : AutoVisitor
     {
-        public List<string> idList =new List<string>();
+        public Dictionary<int, string> IDDict = new Dictionary<int, string>();
 
         public override void VisitIdNode(IdNode id)
         {
-            if (idList.Contains(id.Name)) return; // Если в списке уже есть такое имя: выходим?
-            else idList.Add(id.Name); // Иначе добавляем имя переменной в список?
+            if (IDDict.ContainsValue(id.Name)) return; // Если в словареуже есть такое имя, выходим.
+            else IDDict.Add(IDDict.Count, id.Name); // Иначе добавляем имя переменной в словарь.
         }
     }
 }
