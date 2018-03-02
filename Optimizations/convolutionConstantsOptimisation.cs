1788c58a8c0239a9dae894c1240d22b6c58bfd49
savoptik <sav70792@gmail.com>
2018-03-02 21:40:31 +0300

    добавленн класс, добавленны, но не описаны, методы.

---
+++ Optimizations/convolutionConstantsOptimisation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ThreeAddr;
+
+namespace SimpleLang.Optimizations
+{
+    class convolutionConstantsOptimisation : Optimization
+    {
+        public override bool Optimize(BaseBlock bblock)
+        {
+            
+        }
+        private void recognize(ThreeAddrLine)
+        {
+        }
+        private int calculate(int, int, string)
+        {
+        }
+    }
+}
