5cd44e6a3b3c5fe782a9fdec5601fbab4fc0f52d
savoptik <sav70792@gmail.com>
2018-03-02 22:24:18 +0300

    Описан метод вычислитель

--- Optimizations/convolutionConstantsOptimisation.cs
+++ Optimizations/convolutionConstantsOptimisation.cs
@@ -12,11 +12,22 @@ namespace SimpleLang.Optimizations
         {
             
         }
-        private void recognize(ThreeAddrLine)
+        private void recognize(ThreeAddrLine line) // метод получает строку трехадресного кода, конвертирует операнты и записывает результат.
         {
         }
-        private int calculate(int, int, string)
+        private int calculate(int a, int b, string OpType) // Метод в зависимости от операции выполняет вычисление и возвращает значение.
         {
+            switch (OpType)
+            {
+                case "+": return a + b; // Если найден плюс: верн\тся сумма.
+                case "-": return a - b; // Если минус: то разность.
+                case "*": return a * b;
+                case "/": return a / b;
+                case "<": if (a < b) return 1; else return 0; // Если a меньше b, верн\тся 1: иначе 0.
+                case ">": if (a > b) return 1; else return 0;
+                case "==": if (a == b) return 1; else return 0;
+                default: return 0; // В остальных случаях будет возвращ\н ноль.
+            }
         }
     }
 }
