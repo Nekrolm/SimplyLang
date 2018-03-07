a0ce25bce6af94894f48387d8166a723bb90bfe7
dips3095 <dips3095@gmail.com>
2018-03-07 10:06:41 +0300

    changed some logic

--- Optimizations/AlgebraIdentity.cs
+++ Optimizations/AlgebraIdentity.cs
@@ -17,25 +17,24 @@ namespace SimpleLang.Optimizations
             }
             return answer;
         }
-        private bool Recognize(ThreeAddrLine line) // метод получает строку трехадресного кода, конвертирует операнты и записывает результат.
-        {
-            int a = 0, b = 0;
-            bool isaconst = int.TryParse(line.LeftOp, out a); // Получаем левый оперант.
-            bool isbconst = int.TryParse(line.RightOp, out b);
+		private bool Recognize(ThreeAddrLine line) // метод получает строку трехадресного кода, конвертирует операнты и записывает результат.
+		{
+			int a = 0, b = 0;
+			bool isaconst = int.TryParse(line.LeftOp, out a); // Получаем левый оперант.
+			bool isbconst = int.TryParse(line.RightOp, out b);
 
-            if (line.LeftOp == null)
-            {
-                isaconst = true;
-                a = 0;
-            }
-            string res;
-            if ((isaconst && isbconst) || (!isaconst && !isbconst && (line.LeftOp != line.RightOp))) return false; //если у нас обе константы, или обе переменны, которые не равны друг другу, то не наш случай
+			if (line.LeftOp == null)
+			{
+				isaconst = true;
+				a = 0;
+			}
+			string res=null;
+			if (!isaconst && !isbconst && (line.LeftOp != line.RightOp)) return false; // обе переменны, которые не равны друг другу, то не наш случай
 
-            // Конвертируем операнды и считаем результат
-            if (isaconst && (a == 1 || a == 0)) { res = Compute(line.RightOp, a, line.OpType).ToString();  }
-            else if (isbconst && (b == 1 || b == 0)) { res = Compute(line.LeftOp, b, line.OpType).ToString(); }
-            else if (line.OpType== "/") { res = "1"; }
-            else if (line.OpType == "-") { res = "0"; }
+			// Конвертируем операнды и считаем результат
+			if (!isaconst && !isbconst) { if (line.OpType=="-") res="0"; }
+			if (isaconst && (a == 1 || a == 0)) { res = ComputeVarRigth(a, line.RightOp, line.OpType); }
+			if (isbconst && (b == 1 || b == 0)) { res = ComputeVarLeft(line.LeftOp, b, line.OpType);  }
 
             if (res != null)
             {
@@ -46,21 +45,35 @@ namespace SimpleLang.Optimizations
             }
             return false;
         }
-        private int? Compute(string MyVar, int MyConst, string OpType) // Метод в зависимости от операции выполняет вычисление и возвращает значение.
+        private string ComputeVarLeft(string MyVar, int MyConst, string OpType) // Метод в зависимости от операции выполняет вычисление и возвращает значение.
         {
             switch (OpType)
             {
-                case "+": if (MyConst==0) return MyVar; 
-                case "-": if (MyConst==0) return MyVar; 
-                case "*": if (MyConst == 0) return 0; else return MyVar;
-                case "/": if (MyConst == 1) return MyVar; else return 0;
+                case "+": if (MyConst == 0) return MyVar; else return null;
+                case "-": if (MyConst == 0) return MyVar; else return null;
+                case "*": if (MyConst == 0) return "0"; else return MyVar;
+                case "/": if (MyConst == 1) return MyVar; else return null;
                 case ThreeAddrOpType.And:
-                    if (MyConst==0) return 0;
+					if (MyConst == 0) return "0"; else return null;
                 case ThreeAddrOpType.Or:
-                    if (MyConst == 1) return 1;
-                default:
+                    if (MyConst == 1) return "1"; else return null;
+				default:
                     return null; 
             }
         }
-    }
+		private string ComputeVarRigth(int MyConst, string MyVar, string OpType) // Метод в зависимости от операции выполняет вычисление и возвращает значение.
+		{
+			switch (OpType)
+			{
+				case "+": if (MyConst == 0) return MyVar; else return null;
+				case "*": if (MyConst == 0) return "0"; else return MyVar;
+				case ThreeAddrOpType.And:
+					if (MyConst == 0) return "0"; else return null;
+				case ThreeAddrOpType.Or:
+					if (MyConst == 1) return "1"; else return null;
+				default:
+					return null;
+			}
+		}
+	}
 }
