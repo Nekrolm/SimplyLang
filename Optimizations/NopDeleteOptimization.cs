2939a928d7d7f3206dc3e7401461ffb74782bcbf
seregaworld <seregaworld@mail.ru>
2018-03-14 10:43:00 +0300

    fixes

--- Optimizations/NopDeleteOptimization.cs
+++ Optimizations/NopDeleteOptimization.cs
@@ -12,45 +12,49 @@ namespace SimpleLang.Optimizations
         public override bool Optimize(BaseBlock bblock)
         {
             bool answer = false; // Индикатор того, что хоть один раз, но оптимизация была выполнена.
-            List<int> bb = new List<int>();
+            List<int> bbLabel = new List<int>(); // Здесь будет храниться номера строк блока, в которых nop
             int i = 0;
+            //ищем строки с nop
             foreach (var line in bblock.Code) // Проход по всему базовому блоку.
-            {   
-                 if (line.OpType == "nop")
-                     bb.Add(i);
-                i++;                  
-            }
-            
-            if (bb.Count() != 0)
-                if(bb.Count() == bblock.Code.Count())
-                    bb.RemoveAt(0);
-            if (bb.Count()!=0)
-                answer = true;
-
-            i = 0;
-            foreach (var line in bb) // Проход по всему базовому блоку.
-            { 
-                bblock.Code.RemoveAt(line-i);
+            {
+                if (line.OpType == "nop")
+                    bbLabel.Add(i);
                 i++;
             }
-            if (bb.Count() != 0)
+
+            //если блок состоит из nop, то оставляем только первый nop
+            if ((bblock.Code.Count() != 0) && (bbLabel.Count() == bblock.Code.Count()))
+                bbLabel.RemoveAt(0);
+            //проверяем была ли найдена хоть один nop
+            if (bbLabel.Count() != 0)
             {
-                if (bb[0] != 0)
+                //удаляе строки nop
+                i = 0;
+                foreach (var line in bbLabel) // Проход по всему базовому блоку.
+                {
+                    bblock.Code.RemoveAt(line - i);
+                    i++;
+                }
+                //далее обновляем метки
+                if (bbLabel[0] != 0)
                     i = bblock.StartLabel;
                 else
                 {
                     int val = 0;
-                    while ((bb.Count() > val+1) && ((bb[val + 1] - val) == 1))
-                        val = bb[val + 1];
-                    i = bblock.StartLabel - val-1;
+                    while ((bbLabel.Count() > val + 1) && ((bbLabel[val + 1] - val) == 1))
+                        val = bbLabel[val + 1];
+                    i = bblock.StartLabel - val - 1;
                 }
+                
                 foreach (var line in bblock.Code) // Проход по всему базовому блоку.
                 {
                     line.Label = i.ToString();
+
                     i++;
                 }
-            }
 
+                answer = true;
+            }
             return answer;
         }
     }
