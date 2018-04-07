43a24094f222232bb8a052d0fa9d6d6132a8a7f3
savoptik <sav70792@gmail.com>
2018-04-07 21:31:51 +0300

    Описана структура класса. Методы ещ\ не описаны.

--- Utility/CodePrinter.cs
+++ Utility/CodePrinter.cs
@@ -9,5 +9,11 @@ namespace SimpleLang.Utility
 {
     class CodePrinter
     {
+        private string FileName = new bool; // Имя файла зада\тся конструктором.
+        private bool ModeFlag = new bool; // Флаг режима вывода зада\тся конструктором.
+        private void WriteBinaryFile(List<ThreeAddrLine> LT); // Метод выкинет на диск бинарный файл.
+        private void WriteTextFile(List<ThreeAddrLine> LT); // метод выкинет на диск текстовый файл.
+        CodePrinter(string FileName, bool Mode); // Конструктор установит значения приватных полей.
+        public void Write(List<ThreeAddrLine> LT); // метод в зависимоти от флага запустит необходимый вывод.
     }
 }
