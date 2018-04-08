85bf60faee51327edf3f0213559101d6b7b37802
savoptik <sav70792@gmail.com>
2018-04-08 17:35:10 +0300

    Описан метод вывода кода в текст.

--- Utility/CodePrinter.cs
+++ Utility/CodePrinter.cs
@@ -3,16 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using ThreeAddr
 
 namespace SimpleLang.Utility
 {
     class CodePrinter
     {
-        private string Name = new string(); // Имя файла зада\тся конструктором.
+        private string Name = new string(); // Имя файла задаётся конструктором.
         private bool ModeFlag = new bool(); // Флаг режима вывода задаётся конструктором.
         private void WriteBinaryFile(List<ThreeAddrLine> LT); // Метод выкинет на диск бинарный файл.
-        private void WriteTextFile(List<ThreeAddrLine> LT); // метод выкинет на диск текстовый файл.
+        private void WriteTextFile(List<ThreeAddrLine> LT) // метод выкинет на диск текстовый файл.
+        {
+            string Empty = "    "; // вместа таба.
+            string Space = " "; // пробел.
+            List<string> Code;
+for (int i = 0; i < LT.Count; i++)
+            {
+                string NewLine = new string();
+                switch (LT[i].OpType)
+                {
+                    case ThreeAddrOpType.Nop: { NewLine = LT[i].Label + Space + LT[i].OpType; break; }
+                    case ThreeAddrOpType.Write: { NewLine = LT[i].Label + Space + LT[i].OpType + Space + Empty + Space + Empty + LT[i].RightOp; break; }
+                    case ThreeAddrOpType.Read: { NewLine = LT[i].Label + Space + LT[i].Accum; break; }
+                    case ThreeAddrOpType.IfGoto: { NewLine = LT[i].Label + Space + LT[i].OpType + Space + Empty + Space + LT[i].LeftOp + Space + LT[i].RightOp; break; }
+                    case ThreeAddrOpType.Goto: { NewLine = LT[i].Label + Space + LT[i].OpType + Space + Empty + Space + Empty + LT[i].RightOp; break; }
+                    case ThreeAddrOpType.Not: { NewLine = LT[i].Label + Space + LT[i].OpType + Space + LT[i].Accum + Space + Empty + LT[i].RightOp; break; }
+                    case ThreeAddrOpType.Minus:
+                        {
+                            if (LT[i].LeftOp == null) NewLine = LT[i].Label + Space + LT[i].OpType + Space + LT[i].Accum + Space + Empty + Space + LT[i].RightOp;
+                            else NewLine = LT[i].Label + Space + LT[i].OpType + Space + LT[i].Accum + Space + Empty + Space + LT[i].RightOp;
+                            break;
+                        }
+                    default: NewLine = LT[i].Label + Space + LT[i].OpType + Space + LT[i].Accum + Space + LT[i].LeftOp + Space + LT[i].RightOp;
+                }
+                Code.Add(NewLine);
+            }
+            File.WriteAllLines(Name, Code);
+        }
+
         CodePrinter(string FileName, bool Mode) // Конструктор установит значения приватных полей.
         {
             Name = FileName;
@@ -21,8 +50,8 @@ namespace SimpleLang.Utility
 
         public void Write(List<ThreeAddrLine> LT) // метод в зависимоти от флага запустит необходимый вывод.
         {
-            if (ModeFlag) WriteTextFile(LT); // если есть флаг, пишем текстовый файл.
             WriteBinaryFile(LT); // бинарный пишем в любом случае.
+            if (ModeFlag) WriteTextFile(LT); // если есть флаг, пишем текстовый файл.
         }
     }
 }
